d596543b51d1e477ea6a8984e4a7bdb7a147a188
zhaoyouwen <zhaoyouwen@dgg.net>
2019-11-04 14:49:27 +0800

    屏蔽白米饭和高配及小份餐

--- Order2/Order.xaml.cs
+++ Order2/Order.xaml.cs
@@ -16,6 +16,7 @@ using Artifact.Data;
 using Artifact.Reader;
 using Order2.Entity;
 using Order2.Service;
+using System.Text.RegularExpressions;
 
 namespace Order2
 {
@@ -26,11 +27,11 @@ namespace Order2
     {
         private List<Element> empList;
         private OrderService orderService = new OrderService();
-        private  IniReader cfg;
+        private IniReader cfg;
         private String mealId;
         private delegate void ShowRecordsDelegate(List<String> records);
-        private delegate void AlertOrderResultDelegate(List<String> failedList,List<Element> empList);
-        
+        private delegate void AlertOrderResultDelegate(List<String> failedList, List<Element> empList);
+
 
         public Order()
         {
@@ -55,35 +56,37 @@ namespace Order2
             }
 
 
-            Thread th = new Thread(new ParameterizedThreadStart((args)=> {
-                List<Element> emplyoeeList=args as List<Element>;
+            Thread th = new Thread(new ParameterizedThreadStart((args) =>
+            {
+                List<Element> emplyoeeList = args as List<Element>;
 
                 String areaCode = cfg.Read("setting", "addressId");
-            List<String> failedList=    orderService.OrderMeal(this.mealId, areaCode,emplyoeeList);
-                Dispatcher.BeginInvoke(new AlertOrderResultDelegate((List<String> fl, List<Element> empList)=> {
+                List<String> failedList = orderService.OrderMeal(this.mealId, areaCode, emplyoeeList);
+                Dispatcher.BeginInvoke(new AlertOrderResultDelegate((List<String> fl, List<Element> empList) =>
+                {
                     int failedCount = fl.Count;
                     int total = empList.Count;
                     if (failedCount == 0)
                     {
                         MessageBox.Show("全部订餐成功！");
                     }
-                    else if(failedCount==total)
+                    else if (failedCount == total)
                     {
                         MessageBox.Show("全部订餐失败！");
                     }
                     else
                     {
                         StringBuilder sb = new StringBuilder();
-                        foreach(string name in failedList)
+                        foreach (string name in failedList)
                         {
                             sb.Append("," + name);
                         }
-                        MessageBox.Show(String.Format("{0}订餐失败，其余成功！",sb.Remove(0,1).ToString()),"订餐发生失败!");
+                        MessageBox.Show(String.Format("{0}订餐失败，其余成功！", sb.Remove(0, 1).ToString()), "订餐发生失败!");
 
                     }
 
 
-                }),new object[] { failedList, emplyoeeList });
+                }), new object[] { failedList, emplyoeeList });
 
             }));
             th.Start(emps);
@@ -99,8 +102,8 @@ namespace Order2
             XmlReader reader = new XmlReader();
             reader.Load(System.AppDomain.CurrentDomain.BaseDirectory + "//Resource//employees.xml");
             List<Element> dataList = reader.Select<Element>("//group");
-               
-            foreach(Element group in dataList)
+
+            foreach (Element group in dataList)
             {
                 List<Element> children = reader.Select<Element>("//group[@Name='" + group.Name + "']/*");
                 group.Children = children;
@@ -108,20 +111,23 @@ namespace Order2
 
             this.treeView.ItemsSource = dataList;
             this.empList = dataList;
-      
+
             //模拟登陆
             orderService.getToken();
-      
+
             orderService.Login(cfg.Read("setting", "name"), cfg.Read("setting", "pwd"));
 
             //加载菜单
             String areaCode = cfg.Read("setting", "addressId");
-           List<Meta> meals=  orderService.GetMealList(areaCode);
+            List<Meta> meals = orderService.GetMealList(areaCode);
 
-            string skip = cfg.Read("setting", "skip");
+            string skip = cfg.Read("setting", "skip", "");
+            if (!"".Equals(skip))
+            {
+                meals = meals.FindAll(meal => !Regex.IsMatch(meal["mealName"], skip)&&!"白米饭".Equals(meal["foodName"]));
 
-            meals=   meals.FindAll((meal) => { return skip.IndexOf(meal["mealName"])<0; });
-      
+            }
+
             this.listBox.ItemsSource = meals;
         }
 
@@ -130,11 +136,11 @@ namespace Order2
             CheckBox ckb = sender as CheckBox;
             Element element = ckb.DataContext as Element;
 
-          
-            if (element !=null)
+
+            if (element != null)
             {
-                string type =element.Type;
-            
+                string type = element.Type;
+
                 if (type.Equals("employee"))
                 {
                     Element parent = element.Parent;
@@ -142,7 +148,7 @@ namespace Order2
                     int count = 0;
                     foreach (Element brother in brothers)
                     {
-                        if (brother.Checked!=null && brother.Checked.ToLower().Equals("true"))
+                        if (brother.Checked != null && brother.Checked.ToLower().Equals("true"))
                         {
                             count++;
                         }
@@ -161,13 +167,14 @@ namespace Order2
                         parent.Checked = "Vague";
                     }
 
-                }else if (type.Equals("group"))
+                }
+                else if (type.Equals("group"))
                 {
                     List<Element> children = element.Children;
                     foreach (Element child in children)
                     {
-                
-                        child.Checked=   ckb.IsChecked==true?"True":"False";
+
+                        child.Checked = ckb.IsChecked == true ? "True" : "False";
                     }
 
 
@@ -176,7 +183,7 @@ namespace Order2
 
 
             }
-  
+
 
         }
 
@@ -188,7 +195,7 @@ namespace Order2
             {
                 foreach (Element emp in group.Children)
                 {
-                    if (emp.Checked!=null &&  emp.Checked.ToLower().Equals("true"))
+                    if (emp.Checked != null && emp.Checked.ToLower().Equals("true"))
                     {
                         retList.Add(emp);
                     }
@@ -210,10 +217,10 @@ namespace Order2
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-       
+
             Thread th = new Thread(new ThreadStart(doGetRecord));
             th.Start();
-       
+
 
         }
 
@@ -228,15 +235,16 @@ namespace Order2
             }
             List<String> mealRecordList = orderService.GetOrderRecord(empList);
 
-            Dispatcher.BeginInvoke(new ShowRecordsDelegate( (List<String> records)=> {
+            Dispatcher.BeginInvoke(new ShowRecordsDelegate((List<String> records) =>
+            {
                 Records window = new Records(records);
                 window.ShowDialog();
             }), mealRecordList);
 
-          
+
 
         }
 
-    
+
     }
 }
